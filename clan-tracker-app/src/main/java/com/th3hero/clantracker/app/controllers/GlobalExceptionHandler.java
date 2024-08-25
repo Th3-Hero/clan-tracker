c7ad4ad33ce5de736aa7e2d500e8468e7d3dcef7
Hero <gzoo@uzwelo.com>
2024-08-25 12:25:57 -0400

    Added missing exception handler, fixed defaults

--- clan-tracker-app/src/main/java/com/th3hero/clantracker/app/controllers/GlobalExceptionHandler.java
+++ clan-tracker-app/src/main/java/com/th3hero/clantracker/app/controllers/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.th3hero.clantracker.app.controllers;
 
 import com.kseth.development.rest.error.ProblemDetailFactory;
 import com.th3hero.clantracker.app.exceptions.ClanNotFoundException;
+import jakarta.persistence.EntityExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,6 +14,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ClanNotFoundException.class)
     public ProblemDetail clanNotFoundException(ClanNotFoundException e) {
         return ProblemDetailFactory.createProblemDetail(HttpStatus.NOT_FOUND, e);
+    }
+
+    @ExceptionHandler(EntityExistsException.class)
+    public ProblemDetail entityExistsException(EntityExistsException e) {
+        return ProblemDetailFactory.createProblemDetail(HttpStatus.CONFLICT, e);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
--- clan-tracker-app/src/main/java/com/th3hero/clantracker/app/dto/config/ConfigUpload.java
+++ clan-tracker-app/src/main/java/com/th3hero/clantracker/app/dto/config/ConfigUpload.java
@@ -8,8 +8,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 )
 public record ConfigUpload(
     @Schema(description = "How often member activity data is pulled (In hours)", defaultValue = "12") Integer memberActivityUpdateInterval,
-    @Schema(description = "Default threshold for bad performance", defaultValue = "30") Integer performanceThresholdBad,
-    @Schema(description = "Default threshold for poor performance", defaultValue = "40") Integer performanceThresholdPoor,
-    @Schema(description = "Default threshold for good performance", defaultValue = "50") Integer performanceThresholdGood,
+    @Schema(description = "Default threshold for bad performance", defaultValue = "7") Integer performanceThresholdBad,
+    @Schema(description = "Default threshold for poor performance", defaultValue = "7") Integer performanceThresholdPoor,
+    @Schema(description = "Default threshold for good performance", defaultValue = "12") Integer performanceThresholdGood,
     @Schema(description = "Default activity period", defaultValue = "28") Integer defaultActivitySummaryDateRange
 ) { }
